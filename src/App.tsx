a384ab0397a8ac83990bc6ffbab756449739a825
Alon-Fabio <alon.the.fabio@gmail.com>
2022-06-29 17:01:20 +0300

    Box parallax

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import "./cssElements/ParallaxScrolling/ParallaxScrolling.css";
 import NavBar from "./Components/AppearingNavBarSim/NavBar/Navbar";
 import UserLopFix from "./Learning/UserLoping/UserLopFix";
 import UserLopBug from "./Learning/UserLoping/UserLopBug";
+import CloverIMG from "./cssElements/ParallaxScrolling/Images/Clover.png";
+import BohkeIMG from "./cssElements/ParallaxScrolling/Images/Bohke.png";
 
 function App() {
   const [route, setRoute] = useState("");
@@ -13,10 +15,32 @@ function App() {
   };
   return (
     <div className="App">
-      <header>
-        <NavBar onRouteChange={onRouteChange} />
-      </header>
-      <section>
+      <NavBar onRouteChange={onRouteChange} />
+
+      <section className="ParallaxContainer">
+        <div className="mainBG"></div>
+        <img src={CloverIMG} alt="main2" className="main2" />
+        <img src={BohkeIMG} alt="mainFG" className="mainFG" />
+      </section>
+      <section
+        className="ParallaxContainer"
+        style={{ justifyContent: "center" }}
+      >
+        <section className="ParallaxContainer boxContainer">
+          <div className="box front">1</div>
+          <div className="box back">2</div>
+          <div className="box up">3</div>
+          <div className="box down">4</div>
+          <div className="box left">5</div>
+          <div className="box right">6</div>
+        </section>
+      </section>
+      <section style={{ backgroundColor: "#333" }}>
+        <h1>Learning</h1>
+        <UserLopBug />
+        <UserLopFix />
+      </section>
+      <section style={{ backgroundColor: "#333" }}>
         <h1>Learning</h1>
         <UserLopBug />
         <UserLopFix />
